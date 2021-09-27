5371ab1c321efad82844e37240be869a20400c6f
frawebdev <fra@frawebdev.com>
2021-09-27 09:07:32 +0200

    second commit

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 //run when client connects
 io.on('connection', socket => {
     console.log('websocket connection')
-
-    socket.emit('message', 'welcome')
 })
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT)
+server.listen(PORT, () => console.log('port: ' + PORT))
